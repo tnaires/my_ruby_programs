3bf4ed1aed283daca6c7234f277066cd55e23733
tnaires <tnaires@gmail.com>
2012-02-05 12:49:15 -0200

    Day 7: modifying to use rack gem

--- simple_rack.rb
+++ simple_rack.rb
@@ -1,2 +1,4 @@
+require 'rack'
+
 rack_app = lambda {|env| [200, {"Content-Type" => "text/plain"}, ["Command line argument you typed was: #{ARGV[0]}"]]}
-puts rack_app.call({})
+Rack::Handler::WEBrick.run rack_app, :Port => 8500
